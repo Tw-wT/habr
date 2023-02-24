9c3bfca2960bd077987e3b04a5d8f0c5932a6c88
Bogdan <bogdan_hmaruk@mail.ru>
2023-02-24 09:54:21 +0300

    add redux store, add first entity

---
+++ src/app/entities/Counter/index.ts
@@ -0,0 +1,9 @@
+import { Counter } from './ui/Counter';
+import { counterReducer } from './model/slice/counterSlice';
+import type { CounterSchema } from './model/types/counerSchema';
+
+export {
+    Counter,
+    counterReducer,
+    CounterSchema,
+};
